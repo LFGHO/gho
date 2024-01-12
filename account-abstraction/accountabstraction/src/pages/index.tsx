7495728e011b37ed38b30f9d49aa16a54a30ffb7
varun <varunshekhar1104@gmail.com>
2024-01-12 23:59:52 +0530

    completed sdk integration

--- account-abstraction/accountabstraction/src/pages/index.tsx
+++ account-abstraction/accountabstraction/src/pages/index.tsx
@@ -3,7 +3,19 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import { ParticleAuthModule, ParticleProvider } from "@biconomy/particle-auth";
+import { useState } from "react";
+import { IBundler, Bundler } from "@biconomy/bundler";
+import {
+  BiconomySmartAccountV2,
+  DEFAULT_ENTRYPOINT_ADDRESS,
+} from "@biconomy/account";
 import { ethers } from "ethers";
+import { ChainId } from "@biconomy/core-types";
+import { IPaymaster, BiconomyPaymaster } from "@biconomy/paymaster";
+import {
+  ECDSAOwnershipValidationModule,
+  DEFAULT_ECDSA_OWNERSHIP_MODULE,
+} from "@biconomy/modules";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,20 +30,60 @@ export default function Home() {
     },
   });
 
+
+  const bundler: IBundler = new Bundler({
+    bundlerUrl: 'kuch daal de',// bundler URL from dashboard use 84531 as chain id if you are following this on base goerli,
+    chainId: ChainId.BASE_GOERLI_TESTNET,
+    entryPointAddress: DEFAULT_ENTRYPOINT_ADDRESS,
+  })
+
+  const paymaster: IPaymaster = new BiconomyPaymaster({
+    paymasterUrl: 'put something',// paymaster url from dashboard
+  })
+
+  const [address, setAddress] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
+  const [smartAccount, setSmartAccount] = useState<BiconomySmartAccountV2 | null>(
+    null,
+  );
+  const [provider, setProvider] = useState<ethers.providers.Provider | null>(
+    null,
+  );
+
   const connect = async () => {
     try {
+      setLoading(true);
       const userInfo = await particle.auth.login();
       console.log("Logged in user:", userInfo);
       const particleProvider = new ParticleProvider(particle.auth);
-      console.log({ particleProvider });
       const web3Provider = new ethers.providers.Web3Provider(
         particleProvider,
         "any",
       );
+      setProvider(web3Provider);
+  
+      const module = await ECDSAOwnershipValidationModule.create({
+        signer: web3Provider.getSigner(),
+        moduleAddress: DEFAULT_ECDSA_OWNERSHIP_MODULE,
+      });
+  
+      let biconomySmartAccount = await BiconomySmartAccountV2.create({
+        chainId: ChainId.POLYGON_MUMBAI,
+        bundler: bundler,
+        paymaster: paymaster,
+        entryPointAddress: DEFAULT_ENTRYPOINT_ADDRESS,
+        defaultValidationModule: module,
+        activeValidationModule: module,
+      });
+      setAddress(await biconomySmartAccount.getAccountAddress());
+      setSmartAccount(biconomySmartAccount);
+      setLoading(false);
     } catch (error) {
       console.error(error);
     }
   };
+
+
   return (
     <>
       <Head>
@@ -39,7 +91,11 @@ export default function Home() {
         <meta name="description" content="Based Account Abstraction" />
       </Head>
       <main className={styles.main}>
-
+        <h1>Based Account Abstraction</h1>
+        <h2>Connect and Mint your AA powered NFT now</h2>
+        {!loading && !address && <button onClick={connect} className={styles.connect}>Connect to Based Web3</button>}
+        {loading && <p>Loading Smart Account...</p>}
+        {address && <h2>Smart Account: {address}</h2>}
       </main>
     </>
   )
