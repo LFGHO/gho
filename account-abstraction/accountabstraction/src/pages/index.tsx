e49bee7aeb32ac8b4efd4c0be99484fb59d9f123
varun <varunshekhar1104@gmail.com>
2024-01-12 23:52:01 +0530

    set up particle auth

--- account-abstraction/accountabstraction/src/pages/index.tsx
+++ account-abstraction/accountabstraction/src/pages/index.tsx
@@ -2,10 +2,36 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import { ParticleAuthModule, ParticleProvider } from "@biconomy/particle-auth";
+import { ethers } from "ethers";
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const particle = new ParticleAuthModule.ParticleNetwork({
+    projectId: "",
+    clientKey: "",
+    appId: "",
+    wallet: {
+      displayWalletEntry: true,
+      defaultWalletEntryPosition: ParticleAuthModule.WalletEntryPosition.BR,
+    },
+  });
+
+  const connect = async () => {
+    try {
+      const userInfo = await particle.auth.login();
+      console.log("Logged in user:", userInfo);
+      const particleProvider = new ParticleProvider(particle.auth);
+      console.log({ particleProvider });
+      const web3Provider = new ethers.providers.Web3Provider(
+        particleProvider,
+        "any",
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <>
       <Head>
