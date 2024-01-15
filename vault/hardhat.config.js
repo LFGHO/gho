13971e523040e1d5e85dd2eafd2d96a0e7a6c610
Aryan Godara <aryangodara03@gmail.com>
2024-01-16 00:39:05 +0530

    try out vaults with hardhat

---
+++ vault/hardhat.config.js
@@ -0,0 +1,17 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+require('dotenv').config();
+const { API_URL, PRIVATE_KEY } = process.env;
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.19",
+  defaultNetwork: "sepolia",
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
+  },
+};
