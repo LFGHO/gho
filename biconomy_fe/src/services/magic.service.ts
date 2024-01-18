d418bb148d2c6a5d60c5fb7ec95bd6c28e982ab7
Abdelrahman Soliman <bebofit@aucegypt.edu>
2024-01-19 01:01:13 +0200

    integarte account abstraction with alchemy and magic

--- biconomy_fe/src/services/magic.service.ts
+++ biconomy_fe/src/services/magic.service.ts
@@ -1,6 +1,6 @@
 import { Magic } from "magic-sdk";
 import ChainService from "./chain.service";
-
+import { MagicSigner } from "@alchemy/aa-signers/magic";
 const customNode = {
   rpcUrl: import.meta.env.VITE_RPC_URL || "",
   chainId: ChainService.getInstance().chainId,
@@ -9,3 +9,13 @@ const customNode = {
 export default new Magic(import.meta.env.VITE_MAGICLINK_APIKEY || "", {
   network: customNode,
 });
+
+export const createMagicSigner = async () => {
+  const magicSigner = new MagicSigner(
+    new Magic(import.meta.env.VITE_MAGICLINK_APIKEY || "", {
+      network: customNode,
+    })
+  );
+
+  return magicSigner;
+};
