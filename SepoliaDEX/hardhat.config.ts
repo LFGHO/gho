c80be76672aeecdf230b01e9fb329c702f46b0bd
Aryan Godara <aryangodara03@gmail.com>
2024-01-18 18:21:33 +0530

    add sepolia uniswap addresses

---
+++ SepoliaDEX/hardhat.config.ts
@@ -0,0 +1,8 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox-viem";
+
+const config: HardhatUserConfig = {
+  solidity: "0.5.16",
+};
+
+export default config;
