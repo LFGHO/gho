06b4724afe254d795527df45e6d4c36ee4d5d3c2
varun <varunshekhar1104@gmail.com>
2024-01-20 19:55:56 +0530

    made the site functional

--- frontend_main/src/Root.jsx
+++ frontend_main/src/Root.jsx
@@ -1,22 +1,15 @@
 import React, { useState } from "react";
-import { Outlet } from "react-router-dom";
 import { color } from "./theme";
-import Header from "./pages/Header/Header";
-import { AuthContext, ProviderContext } from "./services/context.service";
+import {Outlet} from "react-router-dom";
 
 function Root() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [provider, setProvider] = useState(null);
   return (
     <div style={{ color: color.text, backgroundColor: color.background }}>
-      <AuthContext.Provider value={[isLoggedIn, setIsLoggedIn]}>
-        <ProviderContext.Provider value={[provider, setProvider]}>
    
             <Outlet />
-        
-          {/* <Header /> */}
-        </ProviderContext.Provider>
-      </AuthContext.Provider>
+      {/* <Home/> */}
     </div>
   );
 }
